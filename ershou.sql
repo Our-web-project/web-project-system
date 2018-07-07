661843102da302c54a920b7072478bd71cc8df1c
chengangan <38811124+chengangan@users.noreply.github.com>
2018-07-08 00:09:37 +0800

    Add files via upload

---
+++ ershou.sql
@@ -0,0 +1,256 @@
+﻿-- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
+--
+-- Host: localhost    Database: ershou
+-- ------------------------------------------------------
+-- Server version	5.1.57-community
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `ershou`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ershou` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ershou`;
+
+--
+-- Table structure for table `t_admin`
+--
+
+DROP TABLE IF EXISTS `t_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_admin` (
+  `userId` int(4) unsigned NOT NULL AUTO_INCREMENT,
+  `userName` varchar(50) DEFAULT NULL,
+  `userPw` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_admin`
+--
+
+LOCK TABLES `t_admin` WRITE;
+/*!40000 ALTER TABLE `t_admin` DISABLE KEYS */;
+INSERT INTO `t_admin` VALUES (1,'admin','admin'),(2,'tangjun','123456');
+/*!40000 ALTER TABLE `t_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_catelog`
+--
+
+DROP TABLE IF EXISTS `t_catelog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_catelog` (
+  `catelog_id` int(11) NOT NULL AUTO_INCREMENT,
+  `catelog_name` varchar(50) DEFAULT NULL,
+  `catelog_miaoshu` text,
+  `catelog_del` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`catelog_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_catelog`
+--
+
+LOCK TABLES `t_catelog` WRITE;
+/*!40000 ALTER TABLE `t_catelog` DISABLE KEYS */;
+INSERT INTO `t_catelog` VALUES (5,'二手手机/iphone','','no'),(7,'数码产品/相机','<p>数码产品/相机</p>','no'),(8,'图书/音像/软件','图书/音像/软件','no'),(10,'文体/户外/乐器','请输入内容','no'),(12,'二手电器','<p>这里主要存放二手电器，如电风扇、台灯等。</p>','no'),(100,'其它','请输入内容','no');
+/*!40000 ALTER TABLE `t_catelog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_gonggao`
+--
+
+DROP TABLE IF EXISTS `t_gonggao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_gonggao` (
+  `gonggao_id` int(11) NOT NULL AUTO_INCREMENT,
+  `gonggao_title` varchar(50) DEFAULT NULL,
+  `gonggao_content` text,
+  `gonggao_data` varchar(50) DEFAULT NULL,
+  `gonggao_fabuzhe` varchar(255) DEFAULT NULL,
+  `gonggao_del` varchar(50) DEFAULT NULL,
+  `gonggao_one1` varchar(50) DEFAULT NULL,
+  `gonggao_one2` varchar(50) DEFAULT NULL,
+  `gonggao_one3` varchar(50) DEFAULT NULL,
+  `gonggao_one4` varchar(50) DEFAULT NULL,
+  `gonggao_one5` datetime DEFAULT NULL,
+  `gonggao_one6` datetime DEFAULT NULL,
+  `gonggao_one7` int(11) DEFAULT NULL,
+  `gonggao_one8` int(11) DEFAULT NULL,
+  PRIMARY KEY (`gonggao_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_gonggao`
+--
+
+LOCK TABLES `t_gonggao` WRITE;
+/*!40000 ALTER TABLE `t_gonggao` DISABLE KEYS */;
+INSERT INTO `t_gonggao` VALUES (1,'重要提示','在本网站购买任何商品，都请不要先支付现金，请当面交易，本网站只提供交易平台，如出现任何法律问题，与本站无关。','2018-4-9 8:15:46',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,'关于网站维护时间问题','本网站将于是2018年，10月1日晚婚晚育12上维护，大约要半小时，给大家带来的不便请原谅。。','2018-4-9 8:20:15',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `t_gonggao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_goods`
+--
+
+DROP TABLE IF EXISTS `t_goods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_goods` (
+  `goods_id` int(11) NOT NULL AUTO_INCREMENT,
+  `goods_name` varchar(50) DEFAULT NULL,
+  `goods_miaoshu` text,
+  `fujian` varchar(50) DEFAULT NULL,
+  `goods_dijia` int(11) DEFAULT NULL,
+  `goods_catelog_id` int(11) DEFAULT NULL,
+  `goods_user_id` int(11) DEFAULT NULL,
+  `goods_shijian` varchar(50) DEFAULT NULL,
+  `goods_del` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`goods_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_goods`
+--
+
+LOCK TABLES `t_goods` WRITE;
+/*!40000 ALTER TABLE `t_goods` DISABLE KEYS */;
+INSERT INTO `t_goods` VALUES (23,'好手机','请输入内容','/upload/1338564479125.jpg',1000,5,1,'2018-3-1 23:28:04','no'),(24,'好电脑','请输入内容地方工业&nbsp;&nbsp;','/upload/1338615819109.jpg',12222,12,1,'2018-3-2 13:43:45','yes'),(25,'iphone5','<p>Iphone5</p>','/upload/1351690115606.jpg',6666,5,8,'2018-3-31 21:28:47','no');
+/*!40000 ALTER TABLE `t_goods` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_jingpai`
+--
+
+DROP TABLE IF EXISTS `t_jingpai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_jingpai` (
+  `jingpai_id` int(11) NOT NULL AUTO_INCREMENT,
+  `jingpai_jiage` int(11) DEFAULT NULL,
+  `jingpai_goods_id` int(11) DEFAULT NULL,
+  `jingpai_user_id` int(11) DEFAULT NULL,
+  `jingpai_shijian` varchar(50) DEFAULT NULL,
+  `del` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`jingpai_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_jingpai`
+--
+
+LOCK TABLES `t_jingpai` WRITE;
+/*!40000 ALTER TABLE `t_jingpai` DISABLE KEYS */;
+INSERT INTO `t_jingpai` VALUES (12,0,25,1,'2018-3-1 23:29:47','no'),(13,0,25,1,'2018-3-1 23:31:57','no'),(14,0,25,1,'2018-3-30 22:38:53','no'),(15,0,24,1,'2018-3-30 22:38:53','no');
+/*!40000 ALTER TABLE `t_jingpai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_liuyan`
+--
+
+DROP TABLE IF EXISTS `t_liuyan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_liuyan` (
+  `liuyan_id` int(11) NOT NULL AUTO_INCREMENT,
+  `liuyan_title` varchar(50) DEFAULT NULL,
+  `liuyan_content` text,
+  `liuyan_date` varchar(50) DEFAULT NULL,
+  `liuyan_user` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`liuyan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_liuyan`
+--
+
+LOCK TABLES `t_liuyan` WRITE;
+/*!40000 ALTER TABLE `t_liuyan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_liuyan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_user`
+--
+
+DROP TABLE IF EXISTS `t_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) DEFAULT NULL,
+  `user_pw` varchar(50) DEFAULT NULL,
+  `user_type` int(11) DEFAULT NULL,
+  `user_realname` varchar(50) DEFAULT NULL,
+  `user_address` varchar(50) DEFAULT NULL,
+  `user_sex` varchar(50) DEFAULT NULL,
+  `user_tel` varchar(50) DEFAULT NULL,
+  `user_email` varchar(50) DEFAULT NULL,
+  `user_qq` varchar(50) DEFAULT NULL,
+  `user_man` varchar(50) DEFAULT NULL,
+  `user_age` varchar(50) DEFAULT NULL,
+  `user_birthday` varchar(50) DEFAULT NULL,
+  `user_xueli` varchar(50) DEFAULT NULL,
+  `user_del` varchar(50) DEFAULT NULL,
+  `user_one1` varchar(50) DEFAULT NULL,
+  `user_one2` varchar(50) DEFAULT NULL,
+  `user_one3` varchar(50) DEFAULT NULL,
+  `user_one4` varchar(50) DEFAULT NULL,
+  `user_one5` varchar(50) DEFAULT NULL,
+  `user_one6` int(11) DEFAULT NULL,
+  `user_one7` int(11) DEFAULT NULL,
+  `user_one8` int(11) DEFAULT NULL,
+  `user_one9` datetime DEFAULT NULL,
+  `user_one10` datetime DEFAULT NULL,
+  `user_one11` bigint(20) DEFAULT NULL,
+  `user_one12` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_user`
+--
+
+LOCK TABLES `t_user` WRITE;
+/*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
+INSERT INTO `t_user` VALUES (1,'tangjun','123456',0,'唐军','中华路27号','男','1521206737','tangjun@163.com','65975062',NULL,NULL,NULL,NULL,'no',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,'yuanmingqi','123456',0,'小唐','中华路27号','男','15281206737','659750682@qq.com','659750682',NULL,NULL,NULL,NULL,'yes',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(7,'huanghai','huanghai',0,'黄海','中华路27号','女','15282827222','tangunnj@163.com','65975929',NULL,NULL,NULL,NULL,'no',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(8,'wwwwww','123456',0,'苏大','苏州','男','12312323','123@qq.com','12321321',NULL,NULL,NULL,NULL,'no',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-3-31 22:19:56
